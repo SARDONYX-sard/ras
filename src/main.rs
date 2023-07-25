2eb325e065002d21b7939ea8ace746b2489f48b6
SARDONYX-sard <68905624+SARDONYX-sard@users.noreply.github.com>
2023-07-26 02:54:03 +0900

    feat!(main): change main fn

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 mod elf;
 mod encoder;
+mod error;
 mod globals;
 mod lexer;
-mod token;
 mod utils;
 
-use std::fs;
-
-use clap::{arg, command, Parser};
-
 use crate::elf::Elf;
-use crate::encoder::Encoder;
-use crate::lexer::Lexer;
+use crate::encoder::parse;
+use crate::error::{bail, Result};
+use crate::lexer::tokenize;
+use clap::{arg, command, Parser};
+use std::fs;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// assembly file path name
     #[clap(value_parser)]
-    name: String,
+    file_name: String,
     /// Output file path name
     #[arg(short, long, default_value_t = format!("./out.o"))]
     out_file: String,
@@ -27,23 +26,27 @@ struct Args {
     keep_locals: bool,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
 
-    let file_name = args.name.as_str();
-    let program = fs::read_to_string(file_name).expect("Should have been able to read the file");
+    let program = match fs::read_to_string(args.file_name) {
+        Ok(src) => src,
+        Err(err) => bail!("{err}"),
+    };
+    let tokens = tokenize(&program)?;
+    dbg!(&tokens);
 
-    let mut l = Lexer::new(file_name, program.as_str());
-    let mut en = Encoder::new(&mut l, file_name);
+    parse(tokens)?;
+    // let mut en = Encoder::new(&mut l, file_name);
     // en.encode();
-    en.assign_addresses();
+    // en.assign_addresses();
 
-    let out_file = args.out_file.as_str();
-    let mut e = Elf::new(out_file, args.keep_locals);
+    let mut e = Elf::new(&args.out_file, args.keep_locals);
     e.collect_rela_symbols();
     e.build_symtab_strtab();
     e.rela_text_users();
     e.build_shstrtab();
     e.build_headers();
     e.write_elf();
+    Ok(())
 }
